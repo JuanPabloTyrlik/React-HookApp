7b14b3955fe2bc1734561fbeaba6a17d8de49e5a
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-09-23 18:32:32 -0300

    RealExampleRef Tests

--- src/components/04-useRef/__tests__/RealExampleRef.test.js
+++ src/components/04-useRef/__tests__/RealExampleRef.test.js
@@ -1,1 +1,16 @@
-describe('Tests on RealExampleRef', () => {});
+import React from 'react';
+import { shallow } from 'enzyme';
+import RealExampleRef from '../RealExampleRef';
+import { act } from '@testing-library/react-hooks';
+
+describe('Tests on RealExampleRef', () => {
+    const wrapper = shallow(<RealExampleRef />);
+    test('should render correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+    test('should render MultipleCustomHooks', () => {
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('MultipleCustomHooks').exists()).toBe(true);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
